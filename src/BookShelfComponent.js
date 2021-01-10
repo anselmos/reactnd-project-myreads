f0317b55a952ac4448ca160a5e6b122f13a97321
Anselmos <anselmos@users.noreply.github.com>
2021-01-10 17:05:06 +0100

    Adds Book Component.

--- src/BookShelfComponent.js
+++ src/BookShelfComponent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
-
-class ListBooksContent extends React.Component {
+import Book from "./Book";
+class BookShelfComponent extends React.Component {
   render() {
     return (
 
@@ -10,22 +10,7 @@ class ListBooksContent extends React.Component {
         <div className="bookshelf-books">
           <ol className="books-grid">
             <li>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="move" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">To Kill a Mockingbird</div>
-                <div className="book-authors">Harper Lee</div>
-              </div>
+              <Book bookTitle={"To Kill a Mockingbird"} bookAuthors={"Harper Lee"}/>
             </li>
             <li>
               <div className="book">
@@ -52,4 +37,4 @@ class ListBooksContent extends React.Component {
   }
 }
 
-export default ListBooksContent
+export default BookShelfComponent
