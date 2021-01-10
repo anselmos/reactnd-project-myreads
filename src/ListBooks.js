82035a8deadd59c44caa8bde7f92e0ba9c5615dc
Anselmos <anselmos@users.noreply.github.com>
2021-01-10 17:57:15 +0100

    Removes redundant code.

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -15,19 +15,7 @@ class ListBooks extends React.Component {
           <div className="list-books-title">
             <h1>MyReads</h1>
           </div>
-          <ListBooksContent booksCurrentlyReading={
-            [
-                {
-                bookTitle: 'To Kill A Mocking Bird',
-                bookAuthors: 'Harper Lee'
-                },                {
-                bookTitle: 'To Kill A Mockinaag Bird',
-                bookAuthors: 'Harper Lee'
-                }
-            ]
-          }
-            books={this.props.books}
-          />
+          <ListBooksContent books={this.props.books} />
           <div className="open-search">
             <button onClick={this.handleAddBook.bind(this)}>Add a book</button>
           </div>
