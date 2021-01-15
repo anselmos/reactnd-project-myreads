81d4701b34ff2fe9bbba3646561611cd2109775c
Anselmos <anselmos@users.noreply.github.com>
2021-01-15 23:31:34 +0100

    Updates

---
+++ precache-manifest.d20ee6767262fcb8efe8f1f66e086bff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c812e339b19abf473513",
+    "url": "/reactnd-project-myreads/static/css/main.2e0a9d3a.chunk.css"
+  },
+  {
+    "revision": "c812e339b19abf473513",
+    "url": "/reactnd-project-myreads/static/js/main.c812e339.chunk.js"
+  },
+  {
+    "revision": "0ab53cdae20ba8ad4d80",
+    "url": "/reactnd-project-myreads/static/js/1.0ab53cda.chunk.js"
+  },
+  {
+    "revision": "d2f8cf40e53ea2d3746f",
+    "url": "/reactnd-project-myreads/static/js/runtime~main.d2f8cf40.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/reactnd-project-myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/reactnd-project-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/reactnd-project-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "4c38a9544a6e2d31747619d59e8953cc",
+    "url": "/reactnd-project-myreads/index.html"
+  }
+];
