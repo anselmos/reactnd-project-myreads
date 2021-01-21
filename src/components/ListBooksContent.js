e8b20cdd0870ddb49e0be06a71cdca0fc07d86ae
Anselmos <anselmos@users.noreply.github.com>
2021-01-21 15:35:04 +0100

    Refactors a lot of repeated code with a nice map arrow function that improves extensibility of the bookshelfs.

--- src/components/ListBooksContent.js
+++ src/components/ListBooksContent.js
@@ -2,46 +2,41 @@ import React from "react";
 import "../App.css";
 import BookShelf from "./BookShelf";
 
+const SHELVES = [
+  {
+    title: 'Currently Reading',
+    id: 'currentlyReading'
+  },
+  {
+    title: 'Want To Read',
+    id: 'wantToRead'
+  },
+  {
+    title: 'Read',
+    id: 'read'
+  }
+];
+
 class ListBooksContent extends React.Component {
   filter_books_dict_by_shelf(data, shelf) {
     return Object.fromEntries(
       Object.entries(data).filter(([k, v]) => v.shelf === shelf)
     );
   }
+
   render() {
-    let currentlyReadingBooks = this.filter_books_dict_by_shelf(
-      this.props.books,
-      "currentlyReading"
-    );
-    let wantToReadBooks = this.filter_books_dict_by_shelf(
-      this.props.books,
-      "wantToRead"
-    );
-    let readBooks = this.filter_books_dict_by_shelf(this.props.books, "read");
+    let bookshelfs = SHELVES.map(({title, id}, _) => (
+      <BookShelf
+        handleBookUpdateCallback={this.props.handleBookUpdateCallback.bind(this)}
+        name={title}
+        books={this.filter_books_dict_by_shelf(this.props.books, id)}
+      />
+    ))
     return (
       <div className="list-books-content">
         <div>
-          <BookShelf
-            handleBookUpdateCallback={this.props.handleBookUpdateCallback.bind(
-              this
-            )}
-            name={"Currently Reading"}
-            books={currentlyReadingBooks}
-          />
-          <BookShelf
-            handleBookUpdateCallback={this.props.handleBookUpdateCallback.bind(
-              this
-            )}
-            name={"Want to Read"}
-            books={wantToReadBooks}
-          />
-          <BookShelf
-            handleBookUpdateCallback={this.props.handleBookUpdateCallback.bind(
-              this
-            )}
-            name={"Read"}
-            books={readBooks}
-          />
+          {bookshelfs}
+
         </div>
       </div>
     );
