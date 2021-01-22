40a90cc80965b0e0a3b1914bdde58d8e8d4c2632
Anselmos <anselmos@users.noreply.github.com>
2021-01-22 11:04:01 +0100

    Adds Constants. Updates references of SHELVES. Adds test.

--- src/components/ListBooksContent.js
+++ src/components/ListBooksContent.js
@@ -1,21 +1,7 @@
 import React from "react";
 import "../App.css";
 import BookShelf from "./BookShelf";
-
-const SHELVES = [
-  {
-    title: 'Currently Reading',
-    id: 'currentlyReading'
-  },
-  {
-    title: 'Want To Read',
-    id: 'wantToRead'
-  },
-  {
-    title: 'Read',
-    id: 'read'
-  }
-];
+import * as Constants from "../Constants";
 
 class ListBooksContent extends React.Component {
   filter_books_dict_by_shelf(data, shelf) {
@@ -25,17 +11,18 @@ class ListBooksContent extends React.Component {
   }
 
   render() {
-    let bookshelfs = SHELVES.map(({title, id}, _) => (
+    let allBookShelfs = Constants.SHELVES.map(({title, id}, _) => (
       <BookShelf
         handleBookUpdateCallback={this.props.handleBookUpdateCallback.bind(this)}
         name={title}
         books={this.filter_books_dict_by_shelf(this.props.books, id)}
+        key={id}
       />
     ))
     return (
       <div className="list-books-content">
         <div>
-          {bookshelfs}
+          {allBookShelfs}
 
         </div>
       </div>
