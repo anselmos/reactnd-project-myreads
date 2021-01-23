167ad7b48368c8e69d7c4d123841e08d6f3b629d
Anselmos <anselmos@users.noreply.github.com>
2021-01-23 10:54:21 +0100

    Refactors ListBooks into functional compoment.

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -2,25 +2,23 @@ import React from "react";
 import "../App.css";
 import ListBooksContent from "./ListBooksContent";
 
-class ListBooks extends React.Component {
-  render() {
-    return (
-      <div className="list-books">
+const ListBooks  = ({
+books,
+handleBookUpdateCallback,
+handleAddBookCallback,
+}) => (
+    <div className="list-books">
         <div className="list-books-title">
-          <h1>MyReads</h1>
+            <h1>MyReads</h1>
         </div>
         <ListBooksContent
-          books={this.props.books}
-          handleBookUpdateCallback={this.props.handleBookUpdateCallback.bind(
-            this
-          )}
+            books={books}
+            handleBookUpdateCallback={handleBookUpdateCallback}
         />
         <div className="open-search">
-          <button onClick={this.props.handleAddBookCallback}>Add a book</button>
+            <button onClick={handleAddBookCallback}>Add a book</button>
         </div>
-      </div>
-    );
-  }
-}
+    </div>
 
+)
 export default ListBooks;
